b00fbf38e0b440e5cc1fcd1680fd139d20e0e456
Anurag11223344 <anuragsrivastava48946001@gmail.com>
2024-08-30 06:08:43 +0530

    Managing the directories and files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Landing from './Landing'
+import Home from '../pages/Home';
 
 export default function App() {
   return (
-    <div className='text-red-600'>
-      App
-      <Landing/>
-    </div>
+    // <div className='text-red-600'>
+    //   App
+    
+    // </div>
+    <BrowserRouter>
+    {/* <Landing/> */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import BubbleSort from './Sorting/BubbleSort'
+import React from 'react';
 import { Link } from 'react-router-dom';
-
 
 export default function Landing() {
   return (
     <div> 
-        <a href="../Sorting/BubbleSort"><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Sorting Algorithms</button></a>
-        <br /> <br />
-        <a href="#"><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Searching Algorithms</button></a>
+        <h1 className='text-red-600'>Hello</h1>
+        <Link to="/home">
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            Go to Home
+          </button>
+        </Link>
+        {/* Add more links as needed */}
     </div>
   )
 }
